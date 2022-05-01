3bb6485fb76b69c343bff8d4f7671be73cc2a6d9
m.piday <kakoi-to.pirat@yandex.ru>
2022-05-01 17:07:57 +0300

    add utils: max

--- utils.js
+++ utils.js
@@ -33,6 +33,14 @@ function tail(arr) {
   return pop(arr, arr[0]);
 }
 
+function max(arr) {
+  if (arr.length == 2) {
+    return arr[0] > arr[1] ? arr[0] : arr[1];
+  }
+  const subMax = max(tail(arr));
+  return arr[0] > subMax ? arr[0] : subMax;
+}
+
 function sum(arr = []) {
   if (arr.length < 1) {
     return 0;
